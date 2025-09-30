9f32b6277ec1028cd51b5433aa38d41d016f33b8
WACD <walejandrocamacho@gmail.com>
2025-09-30 13:03:47 -0600

    W3-4

--- S3-4/routes/index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/menuItems', require('./menuItems'));
-router.use('/orders', require('./orders'));
-
-module.exports = router;
